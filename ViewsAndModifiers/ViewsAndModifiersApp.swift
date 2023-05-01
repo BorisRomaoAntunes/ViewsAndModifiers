112acbce4fa057763bf8955fe26f202bc4788d1e
Boris Romão Antunes <62909074+BorisRomaoAntunes@users.noreply.github.com>
2023-05-01 14:58:42 -0300

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Boris R on 01/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
